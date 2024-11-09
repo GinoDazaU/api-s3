12ce5666cf240e38fa43f6c919696815c58df85f
Gino Daza <155994323+GinoDazaU@users.noreply.github.com>
2024-11-08 21:10:29 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -2,7 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    bucket_name = event['bucket_name']
+    bucket_name = event['body']['bucket_name']
     
     response = s3.create_bucket(
         Bucket=bucket_name,
