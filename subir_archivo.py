9f2cf92deab649d575cfa492d714bac173a8dc0c
Gino Daza <155994323+GinoDazaU@users.noreply.github.com>
2024-11-08 21:11:24 -0500

    Update subir_archivo.py

--- subir_archivo.py
+++ subir_archivo.py
@@ -3,10 +3,10 @@ import base64
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    bucket_name = event['bucket_name']
-    directory_name = event['directory_name']
-    file_name = event['file_name']
-    file_content = base64.b64decode(event['file_content'])
+    bucket_name = event['body']['bucket_name']
+    directory_name = event['body']['directory_name']
+    file_name = event['body']['file_name']
+    file_content = base64.b64decode(event['body']['file_content'])
     s3.put_object(Bucket=bucket_name, Key=f'{directory_name}/{file_name}', Body=file_content)
     return {
         'statusCode': 200,
