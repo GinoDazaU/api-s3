c26969a5a72ad575bdeec913ddcc7a4f08620ccf
Gino Daza <155994323+GinoDazaU@users.noreply.github.com>
2024-11-08 21:10:49 -0500

    Update crear_directorio.py

--- crear_directorio.py
+++ crear_directorio.py
@@ -2,8 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    bucket_name = event['bucket_name']
-    directory_name = event['directory_name']
+    bucket_name = event['body']['bucket_name']
+    directory_name = event['body']['directory_name']
     s3.put_object(Bucket=bucket_name, Key=(directory_name + '/'))
     return {
         'statusCode': 200,
